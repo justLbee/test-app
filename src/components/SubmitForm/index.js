49274eccd9aeb3e020ab5a88f17ec01294849133
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2019-10-31 17:33:37 +0300

    Form Добавлены элементы формы, небольшая стилизация

--- src/components/SubmitForm/index.js
+++ src/components/SubmitForm/index.js
@@ -3,8 +3,15 @@ import React, {Component} from "react";
 export default class SubmitForm extends Component {
     render() {
         return (
-            <div>
-                FORM
+            <div className={'form-window-wrapper'}>
+                <h1>Заголовок формы</h1>
+                <form className={'form-wrapper'}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="submit"></input>
+                </form>
+
             </div>
         )
     }
