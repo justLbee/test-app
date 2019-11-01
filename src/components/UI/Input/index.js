38d5715d0aa248ad4b37e783cb3645b11ea101d4
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2019-11-01 12:31:57 +0300

    Input Стилистика валидации

--- src/components/UI/Input/index.js
+++ src/components/UI/Input/index.js
@@ -1,22 +1,32 @@
 import React   from "react";
 import classes from "./input.module.scss"
 
+function isInvalid({valid, touched, shouldValidate}) {
+    return !valid && shouldValidate && touched;
+}
+
 const Input = props => {
     const inputType = props.type || 'text;';
     const htmlFor = `${ inputType }-${ Math.random }`;
 
-    const inputClasses = [classes.inpt];
+    const cls = [classes.InputWrapper];
+
+    if (isInvalid(props)) {
+        cls.push(classes.invalid);
+    }
 
     return (
-        <div className={ classes.InputWrapper }>
+        <div className={ cls.join(' ') }>
             <input
                 type={ inputType }
                 id={ htmlFor }
                 value={ props.value }
                 onChange={ props.onChange }
                 placeholder={ props.placeholder }
-                className={ inputClasses }
             />
+            {
+                isInvalid(props) ? <span>{ props.errorMessage || 'Неверные данные' }</span> : null
+            }
         </div>
     )
 };
