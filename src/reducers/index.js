803062ee4389f12919474f9e0235fcad04c87680
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2019-11-06 12:48:52 +0300

    Notice icon стилизована

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 
 import inputReducer  from "./input-reducer"
-import submitReducer from "./submit-reducer"
 
 const allReducers = combineReducers({
     inputReducer,
